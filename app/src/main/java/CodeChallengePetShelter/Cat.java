f3b2adeb174793da69d9c63dc03c9d43152babc6
Austin Cumberlander <austincumberlander@gmail.com>
2021-08-19 17:57:31 -0500

    Project completed

---
+++ app/src/main/java/CodeChallengePetShelter/Cat.java
@@ -0,0 +1,11 @@
+package CodeChallengePetShelter;
+
+public class Cat extends Animal {
+    Cat() {
+        super("Cat");
+    }
+    @Override
+    public String toString() {
+        return "I'm a cat.";
+    }
+}
